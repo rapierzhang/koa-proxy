eda7881fac055d3d4d0f77d6a71207d34494b746
ZhangMingyang <zhangmingyang@huli.com>
2017-07-31 17:53:10 +0800

    表单完成

--- modules/admin/index.js
+++ modules/admin/index.js
@@ -19,48 +19,72 @@ router.get('/login', async (ctx, next) => {
   });
 });
 
-router.get('/module/urlList', async (ctx, next) => {
-  await ctx.render('module/urlList/index', {
-    urlList: [
-      {
-        title: '测试',
-        id: 1,
-        url: '/test1/test2',
-        serverUrl: 'http://www.baidu.com',
-        header: {
-          userId: 'uid'
+router
+  .get('/module/urlList', async (ctx, next) => {
+    await ctx.render('module/urlList/index', {
+      urlList: [
+        {
+          title: '测试',
+          id: 1,
+          url: '/test1/test2',
+          serverUrl: 'http://www.baidu.com',
+          header: {
+            userId: 'uid'
+          },
+          headerAdd: {
+            'add': 'add1'
+          },
+          body: {
+            'asp': 'aspirine'
+          },
+          bodyAdd: {
+            'pre': 'preciou'
+          }
         },
-        headerAdd: {
-          'add': 'add1'
-        },
-        body: {
-          'asp': 'aspirine'
-        },
-        bodyAdd: {
-          'pre': 'preciou'
+        {
+          title: '测试1',
+          id: 2,
+          url: '/test2/test3',
+          serverUrl: 'http://www.qq.com',
+          header: {
+            userId: 'uid1'
+          },
+          headerAdd: {
+            'add2': 'add13'
+          },
+          body: {
+            'asp4': 'aspirine5'
+          },
+          bodyAdd: {
+            'pre6': 'preciou7'
+          }
         }
-      },
-      {
-        title: '测试1',
-        id: 2,
-        url: '/test2/test3',
-        serverUrl: 'http://www.qq.com',
-        header: {
-          userId: 'uid1'
-        },
-        headerAdd: {
-          'add2': 'add13'
-        },
-        body: {
-          'asp4': 'aspirine5'
-        },
-        bodyAdd: {
-          'pre6': 'preciou7'
-        }
-      }
-    ]
+      ]
+    });
+  })
+  .post('/module/urlList', async (ctx, next) => {
+    console.log(ctx.request.body);
+    ctx.body = 111;
   });
-});
+
+router
+  .post('/module/urlListTransfer', async (ctx, next) => {
+    const data = ctx.request.body;
+    console.log(data);
+    const title = data.title;
+    const url = data.url;
+    const serverUrl = data.serverUrl;
+    const headerFieldChange = arrToJson(data, 'headerOldFieldInput', 'headerNewFieldInput');
+    const headerFieldAdd = arrToJson(data, 'headerFieldAddKey', 'headerFieldAddValue');
+    const bodyFieldChange = arrToJson(data, 'bodyOldFieldInput', 'bodyNewFieldInput');
+    const bodyFieldAdd = arrToJson(data, 'bodyFieldAddKey', 'bodyFieldAddValue');
+
+    const json = { title, url, serverUrl, headerFieldChange, headerFieldAdd, bodyFieldChange, bodyFieldAdd };
+
+    console.log(json);
+
+    ctx.redirect('/admin/module/urlList');
+  });
 
 router.get('/module/urlItem/:id', async (ctx, next) => {
   const theUrl = ctx.url;
@@ -71,12 +95,31 @@ router.get('/module/urlItem/:id', async (ctx, next) => {
       id: 1,
       url: '/test1/test2',
       serverUrl: 'http://www.baidu.com',
-      header: [{oldKey: 'userId', newKey: 'uid'}],
-      headerAdd: [{key: 'add', value: 'add1'}],
-      body: [{'asp': 'aspirine'}],
-      bodyAdd: [{'pre': 'preciou'}]
+      header: [{userId: 'uid'}],
+      headerAdd: [{key: 'add1'}],
+      body: [{asp: 'aspirine'}],
+      bodyAdd: [{pre: 'preciou'}]
     }
   });
 });
+
+
+
+function arrToJson(data, field1, field2) {
+  let arr = [];
+  let item = {};
+  if (typeof data[field1] != 'string') {
+    for (let i = 0, j = data[field1].length; i < j; i ++) {
+      if (data[field1][i] && data[field2][i] && data[field1][i] != '' && data[field2][i] != '') {
+        item[data[field1][i]] = data[field2][i];
+      }
+    }
+  } else {
+    if (data[field1] && data[field2] && data[field1] != '' && data[field2] != '') {
+      item[data[field1]] = data[field2];
+    }
+  }
+  return item;
+}
 
 module.exports = router;
---
+++ public/javascripts/urlItem.js
@@ -0,0 +1,47 @@
+function addHeaderChange() {
+  const html = `
+    <div class="header-field-list">
+      <span class="header-old-field">原字段:</span>
+      <input class="header-old-field-input" value="" name="headerOldFieldInput">
+      <span class="header-new-field">新字段:</span>
+      <input class="header-new-field-input" value="" name="headerNewFieldInput">
+    </div>
+  `;
+  $(".header-change-content").append(html);
+}
+
+function addHeaderField() {
+  const html = `
+    <div class="header-field-add-list">
+      <span class="header-field-add-name-key">key:</span>
+      <input class="header-field-add-input" value="" name="headerFieldAddKey">
+      <span class="header-field-add-name-value">value:</span>
+      <input class="header-field-add-input" value="" name="headerFieldAddValue">
+    </div>
+  `;
+  $(".header-field-add-content").append(html);
+}
+
+function addBodyChange() {
+  const html = `
+    <div class="body-field-list">
+      <span class="body-old-field">原字段:</span>
+      <input class="body-old-field-input" value="" name="bodyOldFieldInput">
+      <span class="body-new-field">新字段:</span>
+      <input class="body-new-field-input" value="" name="bodyNewFieldInput">
+    </div>
+  `;
+  $(".body-change-content").append(html);
+}
+
+function addBodyField() {
+  const html = `
+    <div class="body-field-add-list">
+      <span class="body-field-add-name-key">key:</span>
+      <input class="body-field-add-input" value="" name="bodyFieldAddKey">
+      <span class="body-field-add-name-value">value:</span>
+      <input class="body-field-add-input" value="" name="bodyFieldAddValue">
+    </div>
+  `;
+  $(".body-field-add-content").append(html);
+}
